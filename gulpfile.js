e3a9fe41f25fe80abbcfc4b20c430e0aad150b00
Serhii Ralchenko <ralchenkos@gmail.com>
2018-06-16 20:06:37 +0300

    added bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,10 @@ const path = {
     src: {
         template:  'source/template/*.pug',
         style:     'source/style/main.scss',
-        js:        ['source/js/**/*.js'],
+        js:        ['node_modules/jquery/dist/jquery.min.js',
+                    'node_modules/popper.js/dist/popper.min.js',
+                    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+                    'source/js/**/*.js'],
         imagesCSS: 'source/style/images/**/*.*',
         sprite:    'source/style/icons/**/*.png',
         img:       'source/img/**/*.*',
